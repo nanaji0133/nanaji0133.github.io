91dc4c7b4dc7a2c3bda2da20cc717514e97def54
nanaji0133 <nanajisanka5357@gmail.com>
2021-03-07 13:04:59 -0500

    projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import github from '../assets/icons-awe/github.svg'
+import github from '../assets/icons-awe/github.svg';
 const ProjectCard = ({ project: { name, image, deployed_url, github_url } }) => {
     return (
         <div className="projectrCard col-md-6 col-lg-4 my-2">
             <figure className="projectCard__wrapper">
-                <a href={deployed_url} target="_blank" rel="noopener noreferrer" >
-                    <img src={image} alt={name} className="projectCard__image" />
+                <a href={ deployed_url } target="_blank" rel="noopener noreferrer" >
+                    <img src={ image } alt={ name } className="projectCard__image" />
                 </a>
                 <div className="projectCard__title">
-                    <a href={github_url} target="_blank" rel="noopener noreferrer" >
-                        <img src={github} alt="github link" className="projectCard__icon" />
+                    <a href={ github_url } target="_blank" rel="noopener noreferrer" >
+                        { github_url && <img src={ github } alt="github link" className="projectCard__icon" /> }
                     </a>
-                    {name}
+                    { name }
                 </div>
             </figure>
 
--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,82 +1,92 @@
-import tweetme from '../../assets/projects_images/tweetme.PNG'
-import chatapp from '../../assets/projects_images/chatapp.JPG'
-import hardware_store from '../../assets/projects_images/hardware_store.jpg'
-import portfolio from '../../assets/projects_images/portfolio.JPG'
-import new_year from '../../assets/projects_images/new_year.jpg'
-import color_classification from '../../assets/projects_images/color_classification.jpg'
-import blinking from '../../assets/projects_images/blinking.JPG'
-import COVID from '../../assets/projects_images/COVID.jpg'
-import Dev_talks from '../../assets/projects_images/Dev_talks.jpg'
+import blogApp from '../../assets/projects/blogApp.png';
+import calculator from '../../assets/projects/calculator.png';
+import css3 from '../../assets/projects/css3.png';
+import ems from '../../assets/projects/ems.png';
+import github from '../../assets/projects/github.png';
+import meme from '../../assets/projects/meme.png';
+import next from '../../assets/projects/next.png';
+import storyfeed from '../../assets/projects/storyfeed.png';
+import react from '../../assets/projects/react.png';
+import js from "../../assets/projects/js.png"
 
 const data_projects = [
     {
-        name: 'COVID 19 tracker',
-        image: COVID,
-        deployed_url: 'https://covid-19-tracker-by-sumit.web.app/',
-        github_url: 'https://github.com/Dey-Sumit/covid-19-tracker',
-        category: ['react.js']
+        name: 'Storyfeed Landing Page',
+        image: storyfeed,
+        deployed_url: 'https://storyfeed.xyz/',
+        github_url: '',
+        category: ["ReactJs"]
     },
 
     {
-        name: 'Dev Talks',
-        image: Dev_talks,
-        deployed_url: 'https://dev-talks.herokuapp.com/',
-        github_url: 'https://github.com/Dey-Sumit/Dev-talks',
-        category: ['node.js', 'mongoDB', 'react.js']
+        name: 'Calculator',
+        image: calculator,
+        // deployed_url: '',
+        github_url: 'https://github.com/nanaji0133/Calculator_DRF',
+        category: ["HTML5", "CSS3", "Django REST framework"]
     },
 
     {
-        name: 'Realtime Chat App',
-        image: chatapp,
-        deployed_url: 'https://sumit-chat.netlify.app/',
-        github_url: 'https://github.com/Dey-Sumit/chat-app-socket.io-react-node',
-        category: ['node', 'socket.io', 'react']
+        name: 'Blog App',
+        image: blogApp,
+        // deployed_url: 'https://sumit-chat.netlify.app/',
+        github_url: 'https://github.com/nanaji0133/Blog',
+        category: ["Django", "HTML5", "CSS3"]
     },
 
     {
-        name: 'Tweeter Clone',
-        image: tweetme,
-        deployed_url: 'http://sumaxtweetme.pythonanywhere.com/',
-        github_url: 'https://github.com/Dey-Sumit/tweetme',
-        category: ['django', 'react']
+        name: 'Employement Managment Sysytem',
+        image: ems,
+        // deployed_url: 'http://sumaxtweetme.pythonanywhere.com/',
+        github_url: 'https://github.com/nanaji0133/Employement-Managment-System',
+        category: ["Django REST framework"]
     },
 
     {
-        name: 'Shop Website',
-        image: hardware_store,
-        deployed_url: '!#',
-        category: ['html_css', 'vanilla']
+        name: 'GitHub lesson(Useful GitHub Commands)',
+        image: github,
+        // deployed_url: '!#',
+        github_url: "https://github.com/nanaji0133/GIT-COMMANDS",
+        category: ['GitHub']
     },
 
     {
-        name: 'Dev Portfolio',
-        image: portfolio,
-        deployed_url: 'http://suprateem.herokuapp.com/',
-        category: ['vanilla']
+        name: 'Meme Generator',
+        image: meme,
+        // deployed_url: 'http://suprateem.herokuapp.com/',
+        github_url: "https://github.com/nanaji0133/MemeGenerator---React-Js",
+        category: ['ReactJs']
     },
 
     {
-        name: 'Bengali New Year',
-        image: new_year,
-        deployed_url: 'https://dey-sumit.github.io/NewYear/',
-        category: ['vanilla']
+        name: 'NextJs Course',
+        image: next,
+        deployed_url: 'https://github.com/nanaji0133/next.js-course',
+        category: ['NextJs']
     },
 
     {
-        name: 'Color Classification using tf.js',
-        image: color_classification,
-        deployed_url: '!#',
-        github_url: 'https://github.com/Dey-Sumit/color-classification',
-        category: ['node', 'machine_learning']
+        name: 'CSS3 Course',
+        image: css3,
+        // deployed_url: '!#',
+        github_url: 'https://github.com/nanaji0133/CSS-course',
+        category: ['CSS3']
     },
 
     {
-        name: 'Blinking bubbles',
-        image: blinking,
-        deployed_url: 'https://dey-sumit.github.io/Blinking_Bubbles/',
-        github_url: 'https://github.com/Dey-Sumit/Blinking_Bubbles',
-        category: ['processing']
+        name: 'React.js Course',
+        image: react,
+        // deployed_url: 'https://dey-sumit.github.io/Blinking_Bubbles/',
+        github_url: 'https://github.com/nanaji0133/ReactJS-Course',
+        category: ['ReactJs']
+    },
+    {
+        name: 'JavaScript Course',
+        image: js,
+        // deployed_url: 'https://dey-sumit.github.io/Blinking_Bubbles/',
+        github_url: 'https://github.com/nanaji0133/javascript',
+        category: ['JavaScript']
     }
-]
+];
 
 export default data_projects;
